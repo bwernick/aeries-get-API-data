5471ecc2d8ddc5689a354016b8f2b81e1d85b4b0
bradley wernick <bradley.wernick@gmail.com>
2020-10-13 14:53:05 -0700

    changing output method & added getting school codes function

--- data.py
+++ data.py
@@ -1,12 +1,24 @@
 import requests
-import lxml.etree as etree
+import xml.etree.ElementTree as ET
 
-url = "https://demo.aeries.net/aeries/api/v5/schools/"
-headers = {'content-type': 'application/xml',
+baseURL = "https://demo.aeries.net/aeries/api/v5/schools/"
+headers = {'content-type': 'application/json',
             "AERIES-CERT": "477abe9e7d27439681d62f4e0de1f5e1"
           }
-          
-r = requests.get(url, headers=headers)
-#print (r.content)
-x = etree.parse(r.content)
-print(etree.tostring(x, pretty_print=True))
+        
+def makeAPICall(url):
+  return requests.get(url, headers=headers)
+
+#Prints all school codes and school names
+def getSchools():
+  resp = makeAPICall(baseURL)
+  data = resp.json()
+  for entry in data:
+    sc = entry["SchoolCode"]
+    sn = entry["Name"]
+    print(sc, ' - ', sn)
+
+#def getSchools(schoolCode):
+
+if __name__ == "__main__":
+  getSchools()
