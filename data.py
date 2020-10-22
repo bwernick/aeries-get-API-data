a1a26a7488013052bdf264f8edcd682fe4a00c72
bradley wernick <bradley.wernick@gmail.com>
2020-10-22 10:20:10 -0700

    argparse cmd line function calling

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import json
+import argparse
 
 baseURL = "https://demo.aeries.net/aeries/api/v5/schools/" #base url to acess the aeries demo api
 headers = {'content-type': 'application/json',
@@ -11,31 +12,6 @@ path = []
 #simple api call function        
 def makeAPICall(url):
   return requests.get(url, headers=headers)
-
-#Walks through json/dicts that include lists
-#https://stackoverflow.com/a/54000999
-def walk(d):
-  global path
-  for k,v in d.items():
-    if isinstance(v, str) or isinstance(v, int) or isinstance(v, float):
-        path.append(k)
-        print("{} - {}".format(".".join(path), v)) 
-        path.pop()
-    elif v is None:
-        path.append(k)
-        # do something special
-        path.pop()
-    elif isinstance(v, list):
-        path.append(k)
-        for v_int in v:
-          walk(v_int)
-        path.pop()
-    elif isinstance(v, dict):
-        path.append(k)
-        walk(v)
-        path.pop()
-    else:
-        print("###Type {} not recognized: {}.{}={}".format(type(v), ".".join(path),k, v))
 
 #Prints all school codes and school names
 def getSchools():
@@ -48,6 +24,7 @@ def getSchools():
 
 #displays information on a specfic school to the console
 def getSchools_SC(schoolCode):
+  schoolCode = args.schoolCode
   url = baseURL + str(schoolCode)
   resp = makeAPICall(url)
   data = resp.json()
@@ -55,13 +32,15 @@ def getSchools_SC(schoolCode):
 
 #displays the bell schedule on the console
 def getSchoolBellSchedule(schoolCode):
+  schoolCode = args.schoolCode
   url = baseURL + str(schoolCode) + "/BellSchedule"
   resp = makeAPICall(url)
   data = resp.json()
   print(json.dumps(data, indent=4))
 
 #generates a CSV file of all student info from a specfic school
-def getStudentInfoCSV(schoolCode):
+def getStudentInfo_CSV(schoolCode):
+  schoolCode = args.schoolCode
   url = baseURL + str(schoolCode) + "/students"
   resp = makeAPICall(url)
   data = resp.json()
@@ -80,13 +59,17 @@ def getStudentInfoCSV(schoolCode):
 
 #displays a specfic student's information on the console 
 def getStudentInfo_SID(schoolCode, studentID):
+  schoolCode = args.schoolCode
+  schoolCode = args.studentID
   url = baseURL + str(schoolCode) + "/students/" + str(studentID) + "/extended"
   resp = makeAPICall(url)
   data = resp.json()
   print(json.dumps(data, indent=4))
 
-#generates a CSV file of all student info from a specfic school in a specific grade level
+#generates a CSV file of retrived student info from a specfic school in a specific grade level
 def getStudentInfo_GL(schoolCode, gradeLevel):
+  schoolCode = args.schoolCode
+  schoolCode = args.gradeLevel
   url = baseURL + str(schoolCode) + "/students/grade/" + str(gradeLevel) + "/extended"
   resp = makeAPICall(url)
   data = resp.json()
@@ -103,14 +86,38 @@ def getStudentInfo_GL(schoolCode, gradeLevel):
   f.close() 
   print('Wrote ' + str(linecount) + ' students to "students_grade_' + gradeLevel + '.csv"')
 
-#displays a specfic student's information on the console 
-def getStudentInfo_NM(schoolCode, studentNumber):
-  url = baseURL + str(schoolCode) + "/students/sn/" + str(studentNumber) + "/extended"
-  resp = makeAPICall(url)
-  data = resp.json()
-  print(json.dumps(data, indent=4))
+#command line function calling achieved via argparse
+#Reference: https://docs.python.org/3/library/argparse.html, https://stackoverflow.com/a/30669126
+p = argparse.ArgumentParser()
+subparsers = p.add_subparsers()
 
-#main
-if __name__ == "__main__":
-  #@TODO: implement a UI / function selector
-  
+#parser for getSchools_SC
+getSchools_SC_parser = subparsers.add_parser('getSchools_SC')
+getSchools_SC_parser.add_argument('schoolCode')
+getSchools_SC_parser.set_defaults(func=getSchools_SC)
+
+#parser for def getSchoolBellSchedule:
+BellSchedule_parser = subparsers.add_parser('getSchoolBellSchedule')
+BellSchedule_parser.add_argument('schoolCode')
+BellSchedule_parser.set_defaults(func=getSchoolBellSchedule)
+
+#parser for getStudentInfo_CSV
+getStudentInfo_CSV_parser = subparsers.add_parser('getStudentInfo_CSV')
+getStudentInfo_CSV_parser.add_argument('schoolCode')
+getStudentInfo_CSV_parser.set_defaults(func=getStudentInfo_CSV)
+
+#parser for getStudentInfo_SID
+getStudentInfo_SID_parser = subparsers.add_parser('getStudentInfo_SID')
+getStudentInfo_SID_parser.add_argument('schoolCode')
+getStudentInfo_SID_parser.add_argument('studentID')
+getStudentInfo_SID_parser.set_defaults(func=getStudentInfo_SID)
+
+#parser for getStudentInfo_GL
+getStudentInfo_GL_parser = subparsers.add_parser('getStudentInfo_GL')
+getStudentInfo_GL_parser.add_argument('schoolCode')
+getStudentInfo_GL_parser.add_argument('gradeLevel')
+getStudentInfo_GL_parser.set_defaults(func=getStudentInfo_GL)
+
+#do the parse
+args = p.parse_args()
+args.func(args)
